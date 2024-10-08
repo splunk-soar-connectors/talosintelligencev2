221bf7ebc922fb96f80aa22aeb34405a9b4aa163
Scott Odle <sodle@splunk.com>
2024-10-08 14:22:48 -0600

    PAPP-34786 rename app to ciscotalosintelligence

--- talosintelligence_consts.py
+++
@@ -1,6 +0,0 @@
-# Define your constants here
-
-
-ENDPOINT_QUERY_REPUTATION_V3 = "/Talos.Service.URS/QueryReputationV3"
-ENDPOINT_QUERY_AUP_CAT_MAP = "/Talos.Service.URS/QueryAUPCatMap"
-ENDPOINT_QUERY_TAXONOMIES = "/Talos.Service.TTS/QueryTaxonomyCatalogs"
